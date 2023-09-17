93d1074ad5b4cffe2ed7b6155d3a3fdfa3391b20
diegocamposromo <114028843+diegocamposromo@users.noreply.github.com>
2023-09-17 12:38:25 -0600

    Add files via upload

---
+++ Update DownloadAndMove.py
@@ -0,0 +1,53 @@
+import sys
+import time
+import random
+
+import os
+import shutil
+import logging
+
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+from_dir = "/Users/Kuttimma/Downloads"
+
+# Clase Event Hanlder 
+class FileSystemEventHandler(FileSystemEventHandler):
+
+    def on_created(self,event):
+        print(f"¡Oye, {event.scr_path} ha sido creado!")
+
+    def on_deleted(self,event):
+        print(f"¡Lo siento!¡Alguien borró {event.scr_path} ha sido modificado!")
+
+    def on_modified(self,event):
+        print(f"¡Hola!, {event.scr_path} ha sido modificado!")
+    
+    def on_moved(self,event):
+        print(f"Alguien movió {event.scr_path} a {event.dest_path}")
+
+        
+
+
+# Inicia clase Event Handler 
+event_handler = FileEventHandler()
+
+# Inicia Observer
+observer = Observer()
+
+# Programa the Observer
+observer.schedule(event_handler, from_dir, recursive=True)
+
+
+#Comienza el Observer
+observer.start()
+
+
+#5 Escribe una excepción para keyboardInterrupt
+try:
+    while True:
+        time.sleep(2)
+        print("ejecutando...")
+except KeyboardInterrupt:
+    print("¡detenido!")
+    observer.stop()
